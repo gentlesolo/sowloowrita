745429ba074090deff626f9e3cf30a29f72529c6
Ozioma <oziomaokoroafor@gmail.com>
2022-03-09 18:02:49 +0100

    Security config

--- src/main/java/com/sowloo/sowloowrita/security/SecurityConfig.java
+++ src/main/java/com/sowloo/sowloowrita/security/SecurityConfig.java
@@ -90,4 +90,60 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                                                                            connectionFactoryLocator) {
         return new InMemoryUsersConnectionRepository(connectionFactoryLocator);
     }
+
+    /*
+    An async js code to include in the HTML file in order to load the FB SDK on all website's pages
+
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '{your-app-id}',
+      cookie     : true,
+      xfbml      : true,
+      version    : '{api-version}'
+    });
+
+    FB.AppEvents.logPageView();
+
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
+
+Adding the login button ideally right after the opening body tag.
+
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v13.0&appId=1355138698283323&autoLogAppEvents=1" nonce="BAuB3cpv"></script>
+
+Add this code where you want the button to reflect
+
+<div class="fb-login-button" data-width="" data-size="large" data-button-type="continue_with" data-layout="default" data-auto-logout-link="false" data-use-continue-as="false"></div>
+
+
+To check the login status
+
+
+FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+});
+
+
+<fb:login-button
+  scope="public_profile,email"
+  onlogin="checkLoginState();">
+</fb:login-button>
+
+
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
+     */
 }
