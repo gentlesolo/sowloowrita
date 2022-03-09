c61bf9c6e73b1f48f882aa00ceaf6fe9cb1d8b0e
Ozioma <oziomaokoroafor@gmail.com>
2022-03-09 06:38:42 +0100

    Facebook connection sign up

--- src/main/java/com/sowloo/sowloowrita/facebook/FacebookConnectionSignup.java
+++ src/main/java/com/sowloo/sowloowrita/facebook/FacebookConnectionSignup.java
@@ -1,2 +1,21 @@
-package com.sowloo.sowloowrita.facebook;public class FacebookConnectionSignup {
+package com.sowloo.sowloowrita.facebook;
+
+import com.sowloo.sowloowrita.data.models.AppUser;
+import com.sowloo.sowloowrita.data.repository.AppUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.connect.Connection;
+import org.springframework.social.connect.ConnectionSignUp;
+
+public class FacebookConnectionSignup implements ConnectionSignUp {
+    @Autowired
+    private AppUserRepository appUserRepository;
+
+    @Override
+    public String execute(Connection<?> connection) {
+        AppUser user = new AppUser();
+        user.setUsername(connection.getDisplayName());
+        user.setPassword(randomAlphabetic(8));
+        appUserRepository.save(user);
+        return user.getUsername();
+    }
 }
