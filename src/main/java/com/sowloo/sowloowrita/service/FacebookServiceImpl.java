e147598bc2a8fa39c1a7b0bc3783bdef68ab9aa7
Ozioma <oziomaokoroafor@gmail.com>
2022-03-09 06:40:37 +0100

    Facebook service impl

--- src/main/java/com/sowloo/sowloowrita/service/FacebookServiceImpl.java
+++ src/main/java/com/sowloo/sowloowrita/service/FacebookServiceImpl.java
@@ -1,2 +1,12 @@
-package com.sowloo.sowloowrita.service;public class FacebookServiceImpl {
+package com.sowloo.sowloowrita.service;
+
+public class FacebookServiceImpl implements FacebookService{
+    @Override
+    public String loginUser(String fbAccessToken) {
+
+//        var facebookUser = facebookClient.getUser(fbAccessToken);
+
+        return "Hello";
+    }
+
 }
