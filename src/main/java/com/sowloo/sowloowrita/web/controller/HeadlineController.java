024e25b8790fe3f2392820a224b6f2d245f3bcf8
Tosin Akinnusi <akinnusitosinbenedict@gmail.com>
2022-03-13 09:42:14 +0100

    headline controller

--- src/main/java/com/sowloo/sowloowrita/web/controller/HeadlineController.java
+++ src/main/java/com/sowloo/sowloowrita/web/controller/HeadlineController.java
@@ -8,6 +8,9 @@ import com.sowloo.sowloowrita.service.headline.HeadlineService;
 import com.sowloo.sowloowrita.web.exception.BusinessLogicException;
 import com.sowloo.sowloowrita.web.exception.HeadlineDoesNotExistException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,7 +28,10 @@ public class HeadlineController {
 
 
     @GetMapping()
-    public ResponseEntity<?> findAllHeadlines(){
+    public ResponseEntity<?> findAllHeadlines(@RequestParam(name = "page", defaultValue = "0") int page,
+                                              @RequestParam(name = "size", required = false, defaultValue = "30") int pageSize,
+                                              @RequestParam(name = "sort", required = false, defaultValue = "dateCreated") String sortTerm){
+        Pageable pageable = PageRequest.of(page, pageSize, Sort.by(sortTerm).descending());
         List<Headline> headlines = headlineService.getAllHeadlines();
         return new ResponseEntity<>(headlines, HttpStatus.OK);
         //return ResponseEntity.ok().body(headlines);
