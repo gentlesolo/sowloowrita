9df9f73784f8699b41777ba69737a70cbb136e77
Ozioma <oziomaokoroafor@gmail.com>
2022-03-16 18:40:51 +0100

    add User detail service

--- src/main/java/com/sowloo/sowloowrita/service/MyUserDetailsService.java
+++ src/main/java/com/sowloo/sowloowrita/service/MyUserDetailsService.java
@@ -1,2 +1,24 @@
-package com.sowloo.sowloowrita.service;public class MyUserDetailsService {
+package com.sowloo.sowloowrita.service;
+
+import com.sowloo.sowloowrita.data.models.AppUser;
+import com.sowloo.sowloowrita.data.repository.AppUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    AppUserRepository repository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AppUser user = repository.findByUsername(username);
+
+        return new User(user.getUsername(), user.getPassword(), new ArrayList<>());
+    }
 }
