2fbb288694c95998030d9abdf28aaf58d76224d7
Ozioma <oziomaokoroafor@gmail.com>
2022-03-17 20:24:52 +0100

    configure security

--- src/main/java/com/sowloo/sowloowrita/security/SecurityConfig.java
+++ src/main/java/com/sowloo/sowloowrita/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.sowloo.sowloowrita.security;
 import com.sowloo.sowloowrita.facebook.FacebookConnectionSignup;
 import com.sowloo.sowloowrita.facebook.FacebookSignInAdapter;
 import com.sowloo.sowloowrita.filter.CustomAuthenticationFilter;
+import com.sowloo.sowloowrita.filter.CustomAuthorizationFilter;
 import com.sowloo.sowloowrita.service.MyUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +16,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.social.connect.ConnectionFactoryLocator;
 import org.springframework.social.connect.UsersConnectionRepository;
@@ -31,15 +32,10 @@ import static org.springframework.http.HttpMethod.POST;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
-
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     MyUserDetailsService userDetailsService;
-    @Autowired
-    PasswordEncoder passwordEncoder;
-
     @Autowired
     private FacebookConnectionSignup facebookConnectionSignup ;
     @Value("${spring.social.facebook.appSecret}")
@@ -51,7 +47,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth)
             throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptEncoder());
     }
 
     @Override
@@ -59,16 +55,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .cors().and()
                 .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().exceptionHandling().authenticationEntryPoint((request, response, exception)->response.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST).permitAll()
+                .anyRequest().authenticated()
                 .and()
                 .addFilter(new CustomAuthenticationFilter(authenticationManager()))
-                .authorizeRequests()
-                .antMatchers("/api/login/**", "/api/token/refresh/**").permitAll()
-                .antMatchers(GET, "/api/appuser/**").hasAnyAuthority("ROLE_USER")
-                .antMatchers(POST, "/api/appuser/save/**").hasAnyAuthority("ROLE_ADMIN")
-                .antMatchers(HttpMethod.POST, "/facebook/sign-in").permitAll()
-                .anyRequest().authenticated();
+                .addFilter(new CustomAuthorizationFilter(authenticationManager()))
+                .exceptionHandling()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .formLogin();
     }
     @Bean
     public ProviderSignInController providerSignInController() {
@@ -92,59 +89,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new InMemoryUsersConnectionRepository(connectionFactoryLocator);
     }
 
-    /*
-    An async js code to include in the HTML file in order to load the FB SDK on all website's pages
-
-<script>
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '{your-app-id}',
-      cookie     : true,
-      xfbml      : true,
-      version    : '{api-version}'
-    });
-
-    FB.AppEvents.logPageView();
-
-  };
-
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "https://connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-</script>
-
-Adding the login button ideally right after the opening body tag.
-
-<div id="fb-root"></div>
-<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v13.0&appId=1355138698283323&autoLogAppEvents=1" nonce="BAuB3cpv"></script>
-
-Add this code where you want the button to reflect
-
-<div class="fb-login-button" data-width="" data-size="large" data-button-type="continue_with" data-layout="default" data-auto-logout-link="false" data-use-continue-as="false"></div>
-
-
-To check the login status
-
-
-FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-});
-
-
-<fb:login-button
-  scope="public_profile,email"
-  onlogin="checkLoginState();">
-</fb:login-button>
-
-
-function checkLoginState() {
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
-}
-     */
+    @Autowired
+    public PasswordEncoder bCryptEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
